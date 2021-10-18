0aa54473d88ad71e0e56a440101be97243f71985
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2021-10-18 13:48:59 +0530

    Add files via upload

---
+++ C3_SAA1_Template.py
@@ -0,0 +1,18 @@
+# Define a variable "num1" and assign a value to it
+
+# Define a variable "num2" and assign a value to it
+
+# Define a variable "choice" and assign any one value between 1 to 4
+
+# Check if "choice" is equal to 1, then print the sum of "num1" and "num2"
+if 
+  print(               )
+# Check if "choice" is equal to 2, then print the difference between "num1" and "num2"
+if 
+  print(               )
+# Check if "choice" is equal to 3, then print the product of "num1" and "num2"
+if 
+  print(               )
+# Check if "choice" is equal to 4, then print the result of "num1" divided by "num2"
+if 
+  print(               )
